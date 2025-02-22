24131e24f6e1ef3f826e04c7ec114415f66090df
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2025-02-22 12:03:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RDXSHUKLA/SAPNA-MUSIC",
+    "https://github.com/XFLSWAN/RISHU-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
